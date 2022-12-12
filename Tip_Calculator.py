ae66ae4876850e1936c0088346d629bcf69ea95e
Ruhanyat-994 <alruhanyat994@gmail.com>
2022-12-12 15:27:29 +0600

    commiting the modified tip calc

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -3,11 +3,12 @@ tip_percentage = float(input("Enter the tip percentage :"))/100
 tip_amount = food_amount * tip_percentage
 
 total_amount= food_amount+tip_amount
-
+print("-------------------------")
 print(f"🍔 Food_Amount : ${food_amount}\n ")
 
 print(f"🍽️  Tip_Percentage : ${ tip_percentage}\n")
 
 print(f"❤️ Tip_Amount : ${tip_amount}\n")
 
-print(f"💵 Total_Amount : ${total_amount}\n")
+print(f"💵 Total_Amount : ${total_amount}")
+print("-------------------------")
