2c1c927efa8d727f3d67bd61491d1349ee90b293
Ruhanyat-994 <alruhanyat994@gmail.com>
2022-12-14 15:05:03 +0600

    commiting weather to emoji

---
+++ WEATHER_TO_EMOJI.py
@@ -0,0 +1,13 @@
+def weather_to_emoji():
+  wheather = input("Wheather check :")
+  if wheather == "Rain":
+  
+    print("☔")
+  elif wheather == "Thunderstorm":
+    print("⛈️")
+  elif wheather == "Cloudy":
+    print("☁")
+
+  else:
+    print("😎")
+weather_to_emoji()
