d1dc60da0419e1861dfa94b255c1040681758cb6
Ruhanyat-994 <alruhanyat994@gmail.com>
2022-12-22 08:20:38 +0600

    Dic learned

---
+++ dic.py
@@ -0,0 +1,10 @@
+def introducer():
+  person = {
+  "Name":"Ruhanyat",
+     "Shirt":"Black",
+       "Laptop":"HP"
+    }
+  
+  print(f'Hi my name is {person["Name"]}, I am wearing {person["Shirt"]} shirt, i am using {person["Laptop"]} laptop')
+
+introducer()
